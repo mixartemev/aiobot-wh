5f9d29e8edfe466f43b79887b923174d56cf9ff2
Mike <mixartemev@gmail.com>
2024-07-13 12:42:17 +0300

    cli

--- aiobot_wh/__main__.py
+++ aiobot_wh/__main__.py
@@ -1,12 +1,11 @@
 """The main entry point used for CLI."""
 import sys
-from typing import Sequence
 
 
-def main(args: Sequence[str] | None = None) -> None:
+def main(args: list[str]) -> None:
     """CLI - The main function called as entry point."""
     return print('args:', args)
 
 
 if __name__ == "__main__":
-    sys.exit(main())
+    sys.exit(main(sys.argv))
